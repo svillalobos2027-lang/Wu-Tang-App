fa01598d31028a008eb232aed36901da622154c3
Stephanie Villalobos <svillalobos2027@gmail.com>
2026-04-14 11:17:54 -0700

    April 14 day, only got to eight minutes into it

--- Wu-Tang App/ContentView.swift
+++ Wu-Tang App/ContentView.swift
@@ -8,14 +8,48 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var enteredName = ""
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Wu-Tang\nCoder Name Generator")
+                .font(.title)
+                .fontWeight(.black)
+                .foregroundStyle(.yellow)
+                .multilineTextAlignment(.center)
+                .frame(maxWidth: .infinity)
+                .padding(.bottom)
+                .background(.black)
+            
+            Spacer()
+            TextField("Enter name here", text: $enteredName)
+                .font(.title2)
+                .textFieldStyle(.roundedBorder)
+                .overlay {
+                    RoundedRectangle(cornerRadius: 5)
+                        .stroke(.gray, lineWidth: 1)
+                }
+                .padding()
+            Button {
+                //TODO: Action
+            } label: {
+                Image("wu-tang-button")
+                Text("Get It!")
+            }
+            .font(.title2)
+            .fontWeight(.bold)
+            .foregroundStyle(.yellow)
+            .buttonStyle(.borderedProminent)
+            .tint(.black)
+
+            
+            Spacer()
+            
+            
+            Image("wu-tang")
+                .resizable()
+                .scaledToFit()
         }
-        .padding()
+        
     }
 }
 
